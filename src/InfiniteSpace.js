f7471e621496512a6bcc61963cdf36c99a28b9ac
tyler <tyler_1701@hotmail.com>
2021-10-25 18:28:07 -0400

    nasa api

--- src/InfiniteSpace.js
+++ src/InfiniteSpace.js
@@ -1,27 +1,30 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const API_KEY = process.env.REACT_APP_DEMO_KEY;
+
 export function InfiniteSpace() {
-  const [apods, setApods] = useState([
-    { hello: 'hello world' },
-    { hello: 'sup' },
-  ]);
+  const [apods, setApods] = useState([{ hello: 'hi' }]);
 
-  //   useEffect(()=>{
-  //       const data = axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=${process.env.DEMO_KEY}`)
-  //       console.log(data)
-  //   })
+  // useEffect(()=>{
+  //     const data = axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=${process.env.DEMO_KEY}`)
+  //     console.log(data)
+  // })
 
-  function logs() {
-    console.log(process.env.DEMO_KEY);
+  async function logs() {
+    const { data } = await axios.get(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_KEY}`
+    );
+    setApods(data.photos);
+    console.log(data);
   }
 
   return (
     <>
       <h1>InfiniteSpace here</h1>
       <button onClick={logs}>logs</button>
-      {apods.maps((a) => (
-        <h3>{a.hello}</h3>
+      {apods.map((a) => (
+        <h3>{a.id}</h3>
       ))}
     </>
   );
